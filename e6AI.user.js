226b8de0804e88342e26fa2bdfb30cf51f427814
Lars Antrup <larsantrup@gmail.com>
2025-03-15 21:59:22 +0100

    Added autoplay function for videos

--- e6AI.user.js
+++ e6AI.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         e6AI
 // @namespace    http://tampermonkey.net/
-// @version      2025-03-15
+// @version      2025-03-15 21:59
 // @description  Functions for e6AI / e621
 // @author       Seni
 // @match        https://e6ai.net/posts/*
@@ -10,33 +10,43 @@
 // ==/UserScript==
 
 function clickAddFavButton() {
-    const button = document.getElementById("add-fav-button");
-    if (button) {
-        button.click();
-        // console.log("Button was clicked");
-    } else {
-        console.error("Button with ID 'add-fav-button' not found");
-    }
+  const button = document.getElementById("add-fav-button");
+  if (button)
+    button.click();
+  else
+    console.error("Button with ID 'add-fav-button' not found");
 }
 
 function clickVoteUpButton() {
-    const voteButtons = document.querySelectorAll(".post-vote-up-link span");
-    for (const voteButton of voteButtons) {
-        if (voteButton.classList.contains("score-neutral") && !voteButton.classList.contains("score-positive")) {
-            voteButton.click();
-            // console.log("Vote-up button has been clicked", voteButton);
-            return; // Exit function after the first click
-        }
+  const voteButtons = document.querySelectorAll(".post-vote-up-link span");
+  for (const voteButton of voteButtons) {
+    if (voteButton.classList.contains("score-neutral") && !voteButton.classList.contains("score-positive")) {
+      voteButton.click();
+      return; // Exit function after the first click
     }
+  }
 }
 
 function handleKeyPress(event) {
-    if (event.key.toLowerCase() === "f") {
-        //clickAddFavButton();
-        clickVoteUpButton();
-    }
+  if (event.key.toLowerCase() === "f") {
+    //clickAddFavButton();
+    clickVoteUpButton();
+  }
+}
+
+function setAutoplay() {
+  const video = document.querySelector("video");
+  if (video) {
+      video.autoplay = true;
+      video.muted = true;
+      video.loop = true;
+      video.play();
+  }
 }
 
 // Event Listener
 document.addEventListener("keydown", handleKeyPress);
 document.getElementById("add-fav-button")?.addEventListener("click", clickVoteUpButton);
+document.addEventListener("DOMContentLoaded", function() {
+  setAutoplay();
+});
