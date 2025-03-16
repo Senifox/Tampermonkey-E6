c67a94f83fa7a28ebd134bb8aac98d857ca536d7
Lars Antrup <larsantrup@gmail.com>
2025-03-16 16:23:19 +0100

    Fixed f hotkey for favorites

--- e6AI.user.js
+++ e6AI.user.js
@@ -1,37 +1,98 @@
 // ==UserScript==
 // @name         e6AI
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.3
 // @description  Functions for e6AI / e621
 // @author       Seni
 // @match        https://e6ai.net/posts/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=e6ai.net
 // @grant        none
 // ==/UserScript==
-let version = "1.1";
+let version = "1.3";
+let ignoreKeyPress = false;
 
-function clickAddFavButton() {
-  const button = document.getElementById("add-fav-button");
-  if (button)
-    button.click();
+function favoritePost(shiftKey) {
+  try {
+    ignoreKeyPress = true;
+  let favButton = document.querySelector("#add-fav-button");
+  let removeFavButton = document.querySelector("#remove-fav-button");
+
+  let isFav = document.querySelector(".fav-buttons").classList[1].split('-')[2] == "true";
+
+  addFavButton.removeEventListener("click", voteUp);
+  if (shiftKey)
+  {
+    if (!isFav)
+      return;
+
+    console.log("Removing favorite.");
+    removeFavButton.click();
+  }
   else
-    console.error("Button with ID 'add-fav-button' not found");
-}
+  {
+    if (isFav)
+      return;
 
-function clickVoteUpButton() {
-  const voteButtons = document.querySelectorAll(".post-vote-up-link span");
-  for (const voteButton of voteButtons) {
-    if (voteButton.classList.contains("score-neutral") && !voteButton.classList.contains("score-positive")) {
-      voteButton.click();
-      return; // Exit function after the first click
-    }
+    console.log("Adding favorite.");
+    favButton.click();
+  }
+  
+  voteUp();
+  }
+  finally {
+    ignoreKeyPress = false;
+    setTimeout(() => addFavButton.addEventListener("click", voteUp), 1000);
   }
 }
 
+function voteUp() {
+  document.querySelectorAll(".post-vote-up-link")[1].children[0].click();
+}
+
+function voteDown() {
+  document.querySelectorAll(".post-vote-down-link")[1].children[0].click();
+}
+
+function nextPost() {
+  document.querySelector(".nav-link.next").click();
+}
+
+function previousPost() {
+  document.querySelector(".nav-link.prev").click();
+}
+
 function handleKeyPress(event) {
-  if (event.key.toLowerCase() === "f") {
-    //clickAddFavButton();
-    clickVoteUpButton();
+  if (ignoreKeyPress)
+    return;
+
+  const activeElement = document.activeElement;
+    
+  // Check whether the active element is an input field or a text area
+  if (activeElement.tagName === "INPUT" || activeElement.tagName === "TEXTAREA" || activeElement.isContentEditable) {
+    console.log("Active element is text input. No Key evaluated.");  
+    return; // Function ends early when a text field is active
+  }
+
+  let keyPressed = event.key.toLowerCase();
+  switch(keyPressed)
+  {
+    case "w":
+      voteUp();
+      break;
+    case "s":
+      voteDown();
+      break;
+    case "f":
+      favoritePost(event.shiftKey);
+      break;
+    case "d":
+      nextPost();
+      break;
+    case "a":
+      previousPost();
+      break;
+    default:
+      console.log("(" + keyPressed + ") Key not registered");
   }
 }
 
@@ -48,7 +109,12 @@ function setAutoplay() {
 
 // Event Listener
 setAutoplay();
+let addFavButton = document.getElementById("add-fav-button");
+addFavButton.addEventListener("click", voteUp);
+
+
 document.addEventListener("keydown", handleKeyPress);
-document.getElementById("add-fav-button")?.addEventListener("click", clickVoteUpButton);
+//document.getElementById("add-fav-button")?.addEventListener("click", voteUp);
+//document.getElementById("add-fav-button")?.removeEventListener("click", voteUp);
 
 console.log("Seni's e6 scripts loaded. Version: " + version);
